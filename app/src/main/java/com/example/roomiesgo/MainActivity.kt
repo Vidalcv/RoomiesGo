f5f80b89f19ae60ca048b66ed23ed599057b806f
KarenCoders <kpachecorodriguez1@gmail.com>
2025-06-17 02:42:50 -0600

    Nuevas vistas y modificaciones

--- app/src/main/java/com/example/roomiesgo/MainActivity.kt
+++ app/src/main/java/com/example/roomiesgo/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.roomiesgo.View.AppMainContent
-import com.example.roomiesgo.View.HomeScreen
-import com.example.roomiesgo.View.NewTaskScreen
+
 
 
 class MainActivity : ComponentActivity() {
@@ -14,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NewTaskScreen()
+
             }
 
         }
